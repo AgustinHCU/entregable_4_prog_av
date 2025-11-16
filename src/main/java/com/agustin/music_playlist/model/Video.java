99ea3b20041a90553599896f68e4c9a3a5503482
AgustinHUM <acayrus@correo.um.edu.uy>
2025-11-16 18:20:09 -0300

    indentacion correjida

--- src/main/java/com/agustin/music_playlist/model/Video.java
+++ src/main/java/com/agustin/music_playlist/model/Video.java
@@ -1,67 +1,73 @@
-package src.main.java.com.agustin.music_playlist.model;
+package com.agustin.music_playlist.model;
 
 public class Video {
     private Long id;
     private String title;
     private String url;
-    private boolean isLiked;
+    private int likes;
     private boolean isFavorite;
 
-
-public Video() {
-}
-
-public Video(Long id, String title, String url) {
-    this.id = id;
-    this.title = title;
-    this.url = url;
-    this.isLiked = false;
-    this.isFavorite = false;
-}
-
-public Long getId() {
-    return id;
-}
-public String getTitle() {
-    return title;
-}
-public String getUrl() {
-    return url;
-}
-public boolean isLiked() {
-    return isLiked;
-}
-public void setLiked(boolean liked) {
-    isLiked = liked;
-}
-public boolean isFavorite() {
-    return isFavorite;
-}
-public void setFavorite(boolean favorite) {
-    isFavorite = favorite;
-}
-public void setTitle(String title) {
-    this.title = title;
-}
-public void setUrl(String url) {
-    this.url = url;
-}
-public void setId(Long id) {
-    this.id = id;
-}
-
-public String getEmbedUrl(String url) {
-    String videoId = "";
-    if (url.contains("youtube.com/watch?v=")) {
-        int videoIdIndex = url.indexOf("v=") + 2;
-        videoId = url.substring(videoIdIndex);
+    public Video() {
     }
-    else if (url.contains("youtu.be/")) {
-        int videoIdIndex = url.indexOf("youtu.be/") + 9;
-        videoId = url.substring(videoIdIndex);
-    } 
-    return "https://www.youtube.com/embed/" + videoId;
-}
 
-}
+    public Video(Long id, String title, String url) {
+        this.id = id;
+        this.title = title;
+        this.url = url;
+        this.likes = 0;
+        this.isFavorite = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
     
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getUrl() {
+        return url;
+    }
+    
+    public int getLikes() {
+        return likes;
+    }
+    
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+    
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+    
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmbedUrl() {
+        String videoId = "";
+        if (this.url.contains("youtube.com/watch?v=")) {
+            int videoIdIndex = this.url.indexOf("v=") + 2;
+            videoId = this.url.substring(videoIdIndex);
+        }
+        else if (this.url.contains("youtu.be/")) {
+            int videoIdIndex = this.url.indexOf("youtu.be/") + 9;
+            videoId = this.url.substring(videoIdIndex);
+        } 
+        return "https://www.youtube.com/embed/" + videoId;
+    }
+}
